e638734946112bf0992d3ce18b9290b32b016def
Leonardo Comandini <leo@leocomandini.org>
2020-12-13 10:56:02 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# BEWallet-cli
+
+**WARNING: early stage software, DO NOT USE it with real funds**
+
+BEWallet-cli is a command line interface for Bitcoin and Elements wallets.
+
+BEWallet-cli is based on [BEWallet](https://github.com/LeoComandini/BEWallet).
---
+++ src/main.rs
@@ -0,0 +1,60 @@
+use structopt::StructOpt;
+use bewallet;
+
+/// BEWallet Command Line Interface
+#[derive(Debug, StructOpt)]
+struct BEWalletCliOpt {
+    /// The db path
+    #[structopt(long)]
+    data_root: String,
+    
+    /// Mnemonic
+    #[structopt(long)]
+    mnemonic: String,
+
+    #[structopt(long)]
+    electrum_url: String,
+
+    #[structopt(long)]
+    liquid: bool,
+
+    #[structopt(long)]
+    mainnet: bool,
+
+    #[structopt(long)]
+    development: bool,
+
+    #[structopt(subcommand)]
+    subcommand: BEWalletCliSubcommands,
+}
+
+#[derive(Debug, StructOpt)]
+enum BEWalletCliSubcommands {
+    SyncWallet,
+    GetAddress,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = BEWalletCliOpt::from_args();
+
+    let mut config = bewallet::Network::default();
+    config.electrum_url = Some(args.electrum_url.clone());
+    config.liquid = args.liquid;
+    config.mainnet = args.mainnet;
+    config.development = args.development;
+
+    let wallet = bewallet::ElectrumWallet::new(config.clone(), &args.data_root, &args.mnemonic).unwrap();
+
+    match args.subcommand {
+        BEWalletCliSubcommands::SyncWallet => {
+            println!("Sync: started");
+            wallet.sync().unwrap();
+            println!("Sync: done");
+        },
+        BEWalletCliSubcommands::GetAddress => {
+            let ap = wallet.address().unwrap();
+            println!("Address: {} (pointer: {})", ap.address, ap.pointer);
+        }
+    }
+    Ok(())
+}
